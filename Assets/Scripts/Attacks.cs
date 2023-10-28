668ddb846daa8cbde7b7248353138cf3f66ff16e
ThawWantsRice <ttt4775@rit.edu>
2023-10-28 11:03:47 -0400

    Made a shot Prefab

--- Assets/Scripts/Attacks.cs
+++ Assets/Scripts/Attacks.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AOEExplosion : MonoBehaviour
+public class Attacks : MonoBehaviour
 {
+    [SerializeField]
+    float shotSpeed = 1.0f;
+
     // Start is called before the first frame update
     void Start()
     {
