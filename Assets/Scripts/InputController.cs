0484b230b8a0898242cb7c007ae53e2cffd20519
Jason <c04jason@gmail.com>
2023-10-27 21:56:29 -0400

    .

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -6,11 +6,20 @@ using System;
 
 public class InputController : MonoBehaviour
 {
-    [SerializeField]
-    MovementController movementController;
+    [SerializeField] MovementController movementController;
 
     public void OnMove(InputAction.CallbackContext context)
     {
         movementController.ObjectDirection = context.ReadValue<Vector2>();
+    }
+
+    public void onAim(InputAction.CallbackContext context)
+    {
+        Vector2 mousePosition = context.ReadValue<Vector2>();
+    }
+
+    public void useAbilities(InputAction.CallbackContext context)
+    {
+
     }
 }
