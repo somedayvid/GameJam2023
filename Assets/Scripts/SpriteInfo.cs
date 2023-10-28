21ef245a9830ad6655018114137e8e2177aa0e05
Jason <c04jason@gmail.com>
2023-10-27 21:12:45 -0400

    test commit

--- Assets/Scripts/SpriteInfo.cs
+++ Assets/Scripts/SpriteInfo.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 
 public class SpriteInfo : MonoBehaviour
 {
-    [SerializeField]
-    Vector2 rectSize;
+    [SerializeField] Vector2 rectSize;
 
     [SerializeField]
     new SpriteRenderer renderer;
