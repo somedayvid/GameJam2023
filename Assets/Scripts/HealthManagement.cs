c641b04ffddfa4a3ddb92048dad17133b8060d92
Will Forbes <wforbes1313@gmail.com>
2023-10-28 14:57:08 -0400

    Added Heart Removal on collision

--- Assets/Scripts/HealthManagement.cs
+++ Assets/Scripts/HealthManagement.cs
@@ -8,10 +8,10 @@ public class HealthManagement : MonoBehaviour
     SpriteInfo heart;
     [SerializeField]
     List<SpriteInfo> heartsSpawned = new List<SpriteInfo>();
-    private int totalHearts = 5;
+    private int startingHearts = 5;
     private Vector3 heartLocation;
 
-
+    private CollisionManager collisionManager;
 
     private float screenHeight;
     private float screenWidth;
@@ -22,10 +22,12 @@ public class HealthManagement : MonoBehaviour
         screenHeight = 2f * Camera.main.orthographicSize;
         screenWidth = screenHeight * Camera.main.aspect;
 
+        collisionManager.GetComponent<CollisionManager>();
+
         //Starting Location
         heartLocation = new Vector3((0 - screenWidth / 2 + .5f), (0 + screenHeight / 2 - .5f), 0);
         //Spawn All Hearts
-        for (int i = 0; i < totalHearts; i++)
+        for (int i = 0; i < startingHearts; i++)
         {
             heartsSpawned.Add(Instantiate(heart, heartLocation, Quaternion.identity));
 
@@ -36,6 +38,18 @@ public class HealthManagement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        for (int i = 0;i < heartsSpawned.Count;i++)
+        {
+            if (collisionManager.Colliding)
+            {
+                DestroyHeart(i);
+            }
+        }
+    }
+
+    public void DestroyHeart(int i)
+    {
+        Destroy(heartsSpawned[i].gameObject);
+        heartsSpawned.Remove(heartsSpawned[i]);
     }
 }
