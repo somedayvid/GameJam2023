673ebb7cb26873f27c7583c1d1f935fa38dc7e18
David Gao <david.04.gao@gmail.com>
2023-10-28 13:48:39 -0400

    narwhal enemy movement

--- Assets/Scripts/NarwhalMovement.cs
+++ Assets/Scripts/NarwhalMovement.cs
@@ -5,14 +5,27 @@ using UnityEngine;
 public class NarwhalMovement : MonoBehaviour
 {
     // Start is called before the first frame update
+    private Vector3 direction;
+    [SerializeField]
+    private float velocity;
+    private SpriteRenderer spriteRenderer;
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (transform.position.x < 0)
+        {
+            direction = Vector3.right;
+            spriteRenderer.flipX = true;
+        }
+        else if (transform.position.x > 0)
+        {
+            direction = Vector3.left;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += direction * velocity * Time.deltaTime;
     }
 }
