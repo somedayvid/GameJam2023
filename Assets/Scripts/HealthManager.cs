04d56d143a4b3ffdd31a470bfd5562d9af1e76d7
Jason <c04jason@gmail.com>
2023-10-28 20:59:35 -0400

    Added animation to health

--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -32,8 +32,9 @@ public class HealthManager : MonoBehaviour
     /// <param name="damage"></param>
     public void DamagePlayer(float damage)
     {
-        health -= damage;
-        healthBar.fillAmount = health / 100f;
+        // health -= damage;
+        // healthBar.fillAmount = health / 100f;
+        StartCoroutine(DecreaseHealthOverTime(damage, 2f));
     }
 
     /// <summary>
@@ -46,5 +47,25 @@ public class HealthManager : MonoBehaviour
         health = Mathf.Clamp(health, 0, 100);
 
         healthBar.fillAmount = health / 100f;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="damage"></param>
+    /// <param name="duration"></param>
+    /// <returns></returns>
+    IEnumerator DecreaseHealthOverTime(float damage, float duration)
+    {
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            health -= damage * Time.deltaTime / duration;
+            healthBar.fillAmount = health / 100f;
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
     }
 }
